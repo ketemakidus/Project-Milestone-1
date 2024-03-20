dce09ae6337c40cd606e999a6494bd459a45746e
Kidus Ketema Bitew <ketemakidus009@gmail.com>
2024-03-20 14:06:17 -0400

    node

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const http = require ('http');
+const path = require ('path');
+
+const app = express();
+const port = 3000;
+
+// Middleware to parse incoming request bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files (HTML, CSS, etc.) from the "public" directory
+app.use(express.static('public'));
+
+// Route to handle form submissions
+app.post('/contact.html', (req, res) => {
+    const { name, email, message } = req.body;
+
+    // Process form data send email)
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'ketemakidus009@gmail.com', 
+            pass: 'ruza tmof jzmk sbzt' 
+        }
+    });
+
+    const mailOptions = {
+        from: 'ketemakidus009@gmail.com',
+        to: 'ketemakidus009@gmail.com', 
+        subject: 'New Message from Portfolio Contact Form',
+        text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error('Error occurred:', error);
+            res.status(500).send('Error sending message');
+        } else {
+            console.log('Message sent:', info.response);
+            res.status(200).send('Message sent successfully');
+        }
+    });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
